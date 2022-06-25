e179d09c2ee69baede834e3f7c0becca599062b1
Chris Diehl <cultclassik@gmail.com>
2022-06-25 08:39:09 -0500

    fix comment

--- test/module_test.go
+++ test/module_test.go
@@ -38,7 +38,7 @@ func TestPrivateAksModule(t *testing.T) {
 	// os.Setenv("SKIP_terraform_redeploy", "true")
 	// os.Setenv("SKIP_terraform_destroy", "true")
 
-	# TERRAFORM_CLI_PATH is exported by the HashiCorp Terraform Github Action
+	// TERRAFORM_CLI_PATH is exported by the HashiCorp Terraform Github Action
 	if tfcp := os.Getenv("TERRAFORM_CLI_PATH"); tfcp != "" {
 		terraformBinary = tfcp + "/terraform"
 	}
